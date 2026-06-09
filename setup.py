ca16018eed1f1d807526da2891eb67cd8d9b29b9
q15004040209-creator <q15004040209@gmail.com>
2026-06-09 20:19:09 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="rich_wrap",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Python 终端富文本和美观格式化工具",
+    python_requires=">=3.8",
+)
